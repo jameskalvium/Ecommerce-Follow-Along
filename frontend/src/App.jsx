84cfcd43590070d9d25630c2e72a32f2ca56dc4c
unknown <james.r@kalvium.community>
2024-12-12 13:56:02 +0530

    added signUp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import LoginPage from './components/auth/LoginPage'
-
+import LoginPage from './components/auth/LoginPage.jsx';
+import SignupForm from './components/auth/SignupPage.jsx';
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
-    <div><LoginPage /></div>
-  )
+    <>
+      {/* <LoginPage /> */}
+      {/* <SignupForm /> */}
+      <Routes>
+        <Route path="/" />
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
